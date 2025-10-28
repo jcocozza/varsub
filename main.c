d982d66d787eae0681a6916000f68644bccc6a63
Joseph Cocozza <jcocozza47@gmail.com>
2025-10-28 14:12:41 -0400

    feat: sorta works

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// flags
-// char *var_sep = "";
-// char *assignment_op = "";
-// int batch = 0;
-
-void usage() { printf("help message"); }
 
 typedef struct var {
   char *key;
@@ -171,7 +165,7 @@ void add_token(tokens_t *tkns, token_t tkn) {
 tokens_t *tokenize(tokenizer_t *t) {
   tokens_t *tkns = new_tokens();
 
-  token_t curr_token;
+  token_t curr_token = {0};
   while (curr_token.type != T_END) {
     curr_token = next_token(t);
     add_token(tkns, curr_token);
@@ -216,38 +210,97 @@ vars_t *parse(parser_t *p) {
       } else {
         curr_var.value = curr_token.txt;
         left_of_assignment = 1;
+        // time to flush
+        add_variable(vars, curr_var);
+        curr_var.key = NULL;
+        curr_var.value = NULL;
       }
       break;
     case T_SEP:
-      if (curr_var.key == NULL || curr_var.value == NULL) {
-        fprintf(stderr, "unexpected separator\n");
-        exit(EXIT_FAILURE);
-      }
-      // time to flush
-      add_variable(vars, curr_var);
-      curr_var.key = NULL;
-      curr_var.value = NULL;
       break;
     case T_ASSIGNMENT:
       left_of_assignment = 0;
       break;
     case T_END:
-      fprintf(stderr, "unexpected end\n");
+      fprintf(stderr, "unexpected end. tok: %s\n", curr_token.txt);
       exit(EXIT_FAILURE);
     }
     curr_token = p_advance(p);
-    //printf("tok type: %d: %s\n", curr_token.type, curr_token.txt);
   }
+
+  // for (size_t i = 0; i < vars->cnt; i++) {
+  //   printf("%s:%s\n", vars->variables[i].key, vars->variables[i].value);
+  // }
+
   // flush last var
-  if (curr_var.key == NULL || curr_var.value == NULL) {
-    fprintf(stderr, "unexpected end\n");
-    exit(EXIT_FAILURE);
-  }
-  add_variable(vars, curr_var);
+  // if (curr_var.key == NULL || curr_var.value == NULL) {
+  //  fprintf(stderr, "unexpected remaining var. key: %s, val: %s\n",
+  //          curr_var.key, curr_var.value);
+  //  exit(EXIT_FAILURE);
+  //}
+  // add_variable(vars, curr_var);
   return vars;
 }
 
-char *read_stdin_all(void) {
+char *trim_space(char *s) {
+  size_t size;
+  char *end;
+
+  size = strlen(s);
+
+  if (!size)
+    return s;
+
+  end = s + size - 1;
+  while (end >= s && isspace(*end))
+    end--;
+  *(end + 1) = '\0';
+
+  while (*s && isspace(*s))
+    s++;
+
+  return s;
+}
+
+char *render(vars_t *vars, char *template) {
+  const char *t = template;
+  size_t output_size = strlen(template) + 1;
+  char *output = malloc(output_size);
+  output[0] = '\0';
+
+  while (*t) {
+    char *start = strstr(t, "{{");
+
+    if (!start) {
+      strcat(output, t);
+      break;
+    }
+
+    char *end = strstr(t, "}}");
+    if (!end) {
+      fprintf(stderr, "missing closing }}");
+      exit(EXIT_FAILURE);
+    }
+
+    strncat(output, t, start - t);
+
+    size_t var_len = end - (start + 2);
+    char *var = malloc(var_len + 1);
+    strncpy(var, start + 2, var_len);
+    var[var_len] = '\0';
+
+    var = trim_space(var);
+    char *val = lookup(vars, var);
+
+    output_size = strlen(output) + strlen(val) + 1;
+    output = realloc(output, output_size);
+    strcat(output, val);
+    t = end + 2;
+  }
+  return output;
+}
+
+char *read_all(FILE *f) {
   size_t cap = 4096; // initial capacity (4 KB)
   size_t len = 0;
   char *buf = malloc(cap);
@@ -255,7 +308,7 @@ char *read_stdin_all(void) {
     return NULL;
 
   int c;
-  while ((c = fgetc(stdin)) != EOF) {
+  while ((c = fgetc(f)) != EOF) {
     // grow if needed
     if (len + 1 >= cap) {
       cap *= 2;
@@ -273,27 +326,91 @@ char *read_stdin_all(void) {
   return buf;
 }
 
+// append v onto s connected by delim
+void append(char **s, const char *delim, const char *v) {
+  if (*s == NULL) {
+    *s = malloc(strlen(v) + 1);
+    strcat(*s, v);
+    return;
+  }
+
+  size_t len = strlen(*s) + strlen(delim) + strlen(v);
+  char *tmp = realloc(*s, len + 1);
+  if (!tmp)
+    exit(1);
+  *s = tmp;
+
+  strcat(*s, delim);
+  strcat(*s, v);
+}
+
+// flags
+char *var_sep = "\n";
+char *assignment_op = "=";
+
+void usage() { fprintf(stderr, "usage: varsub [options] [FILE] [vars]"); }
+
 int main(int argc, char *argv[]) {
+  char *template = NULL;
+  char *manually_set = NULL;
+
+  char *input = NULL;
+  int read_input = 0;
   // cli stuff
   for (int i = 1; i < argc; i++) {
     // handle flags
     if (argv[i][0] == '-' && strlen(argv[i]) > 0) {
       if (!strcmp(argv[i], "--help")) {
         return 0;
+      } else if (!strcmp(argv[i], "-s")) {
+        i++;
+        var_sep = argv[i];
+      } else if (!strcmp(argv[i], "-a")) {
+        i++;
+        assignment_op = argv[i];
+      } else if (!strcmp(argv[i], "--set")) {
+        i++;
+        char *newvar = argv[i];
+        append(&manually_set, var_sep, newvar);
+      } else if (!strcmp(argv[i], "--vars")) {
+        i++;
+        FILE *f = fopen(argv[i], "r");
+        if (!f) {
+          fprintf(stderr, "failed to open vars file: %s\n", argv[i]);
+          exit(EXIT_FAILURE);
+        }
+        input = read_all(f);
+        fclose(f);
+        read_input = 1;
       }
+    } else {
+      FILE *f = fopen(argv[i], "r");
+      if (!f) {
+        fprintf(stderr, "failed to open template file: %s\n", argv[i]);
+        exit(EXIT_FAILURE);
+      }
+      template = read_all(f);
+      fclose(f);
     }
   }
 
-  // char *input = read_stdin_all();
+  if (template == NULL || strlen(template) == 0) {
+    fprintf(stderr, "no template found\n");
+    exit(EXIT_FAILURE);
+  }
 
-  char *input = "foo=asdf bar=quix ban=bon bozo=fff";
-  tokenizer_t *tokenizer = new_tokenizer(input, " ", "=");
+  if (!read_input) {
+    input = read_all(stdin);
+  }
+  if (manually_set != NULL) {
+    append(&input, var_sep, manually_set);
+  }
+
+  tokenizer_t *tokenizer = new_tokenizer(input, var_sep, assignment_op);
   tokens_t *tkns = tokenize(tokenizer);
 
   parser_t *p = new_parser(tkns);
   vars_t *v = parse(p);
-
-  for (size_t i = 0; i < v->cnt; i++) {
-    printf("%s:%s\n", v->variables[i].key, v->variables[i].value);
-  }
+  char *output = render(v, template);
+  fprintf(stdout, "%s", output);
 }
